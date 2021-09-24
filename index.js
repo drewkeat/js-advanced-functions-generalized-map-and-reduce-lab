d8f8b1b6279be84c039e1a224e07e30102973ff0
Drew Keat <thedrewkeat@gmail.com>
2021-09-23 20:24:52 -0600

    commit tree 3c2fac5d99e8bd4c8d73799496d43713dbae993c

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(array, work) {
+  let result = [];
+  array.forEach((ele) => result.push(work(ele)));
+  return result;
+}
+
+function reduce(array, work, start) {
+  let result;
+  if (start) {
+    result = start;
+    array.forEach((ele) => result = work(ele, result));
+  } else {
+    result = array[0]
+    for (let i = 1; i < array.length; i++) {
+        result = work(array[i], result)
+    };
+  }
+  return result;
+}
